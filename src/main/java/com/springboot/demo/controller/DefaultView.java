426cbd40b15e59fb2b306546dee03e7a2be33319
Avatar <767808421@163.com>
2018-05-09 00:48:21 +0800

    更改些格式以及注释test，加了个默认方法

---
+++ src/main/java/com/springboot/demo/controller/DefaultView.java
@@ -0,0 +1,31 @@
+package com.springboot.demo.controller;
+
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class DefaultView extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers( ViewControllerRegistry registry ) {
+        registry.addViewController( "/" ).setViewName( "forward:/upload.html" );
+        registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
+        super.addViewControllers( registry );
+    }
+
+//
+//    @Override
+//    public void addViewController(ViewControllerRegistry viewControllerRegistry){
+//
+//    }
+}
--- src/main/java/com/springboot/demo/controller/TestController.java
+++ src/main/java/com/springboot/demo/controller/TestController.java
@@ -3,17 +3,19 @@ package com.springboot.demo.controller;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 public class TestController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "/upload.html";
     }
 
     @RequestMapping("/hello")
@@ -33,5 +35,32 @@ public class TestController {
     @RequestMapping(value = "/swagger",method = RequestMethod.GET)
     public String swagger(){
         return "Hello swagger";
+    }
+
+    @RequestMapping(value = "/uploadFile",method = RequestMethod.POST)
+    public String uploadFile(HttpServletRequest request,
+
+                             @RequestParam("file") MultipartFile file){
+        if(!file.isEmpty()) {
+            //上传文件路径
+            String path = request.getServletContext().getRealPath("/images/");
+            //上传文件名
+            String filename = file.getOriginalFilename();
+            File filepath = new File(path,filename);
+            //判断路径是否存在，如果不存在就创建一个
+            if (!filepath.getParentFile().exists()) {
+                filepath.getParentFile().mkdirs();
+            }
+            //将上传文件保存到一个目标文件当中
+            try {
+                file.transferTo(new File(path + File.separator + filename));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "success";
+        } else {
+            return "error";
+        }
+
     }
 }
